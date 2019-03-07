4f537104aa06448ad4322babb6d831e17321341c
Yuval Assa <assa.yuval107@gmail.com>
2019-03-07 14:16:00 +0200

    feat: Add health check

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { CommentsRouter } from './comments/comments.router';
 import { config } from './config';
+import { HealthRouter } from './utils/health/health.router';
 
 const AppRouter: Router = Router();
 
 AppRouter.use(config.endpoints.comments.api, CommentsRouter);
+AppRouter.use('/health', HealthRouter);
 
 export { AppRouter };
